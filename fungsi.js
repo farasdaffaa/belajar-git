7f75250cd752af76f556ead8c6518939a6dcfb65
Raja Farras Daffa <rajafarras63@gmail.com>
2023-03-24 13:38:42 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,9 @@ console.log('menambahkan file pertama');
 
 const add = (a,b) => a+b;
 console.log(add(1,2))
+
+const subtract = (a,b) => a - b;
+console.log(subtract(5,2));
+
+const multiply = (a,b) => a*b;
+console.log(multiply(6,2));
